73f3d83bdd3956b06f341c0e256cd69a1b39aba3
Mark Weller <markweller1111@gmail.com>
2026-02-16 12:19:00 -0500

    Initial Commit

---
+++ AltCaps/AltCapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltCapsApp.swift
+//  AltCaps
+//
+//  Created by Mark Weller on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct AltCapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AltCapsTests/AltCapsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AltCapsTests.swift
+//  AltCapsTests
+//
+//  Created by Mark Weller on 2/16/26.
+//
+
+import Testing
+@testable import AltCaps
+
+struct AltCapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
